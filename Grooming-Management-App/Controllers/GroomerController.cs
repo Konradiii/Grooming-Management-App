e52133c8a6cf51519318f70af1fec3d67fbf5278
Konrad Karczewski <konrad228833@gmail.com>
2026-08-07 01:40:50 +0200

    Add GroomerController and SalonController

--- Grooming-Management-App/Controllers/GroomerController.cs
+++ Grooming-Management-App/Controllers/GroomerController.cs
@@ -1,6 +1,66 @@
-﻿namespace Grooming_Management_App.Controllers;
+﻿using Grooming_Management_App.DTOs.GroomerDTO;
+using Grooming_Management_App.Services.CurrentUserServ;
+using Grooming_Management_App.Services.GroomerServ;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class GroomerController
+namespace Grooming_Management_App.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GroomerController(IGroomerService service, ICurrentUserService currentUser) : ControllerBase
 {
+    [HttpPut("{id:int}/DeactivateGroomer")]
+    [Authorize(Roles = "Owner")]
+    public async Task<IActionResult> DeactivateGroomer(int id, CancellationToken ct)
+    {
+        var salonId =  currentUser.SalonId;
+        await service.DeactivateGroomerAsync(id, salonId, ct);
+        return NoContent();
+    }
+    [HttpPut("{id:int}/ActivateGroomer")]
+    [Authorize(Roles = "Owner")]
+    public async Task<IActionResult> ActivateGroomer(int id, CancellationToken ct)
+    {
+        var salonId =  currentUser.SalonId;
+        await service.ActivateGroomerAsync(id, salonId, ct);
+        return NoContent();
+    }
+
+    [HttpGet("{id:int}")]
+    [Authorize(Roles = "Owner")]
+    public async Task<GetGroomerDto> GetGroomer(int id, CancellationToken ct)
+    {
+        var salonId = currentUser.SalonId;
+        var groomer = await service.GetGroomerAsync(id, salonId, ct);
+        return groomer;
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Owner")]
+    public async Task<List<GetGroomerDto>> GetAllGroomers(CancellationToken ct)
+    {
+        var salonId = currentUser.SalonId;
+        var groomers = await service.GetAllGroomersAsync(salonId, ct);
+        return groomers;
+    }
+
+    [HttpPut("{id:int}/EditGroomer")]
+    [Authorize(Roles = "Owner")]
+    public async Task<IActionResult> EditGroomer(int id, [FromBody] EditGroomerDto dto, CancellationToken ct)
+    {
+        var salonId = currentUser.SalonId;
+        await service.EditGroomerAsync(dto, id, salonId, ct);
+        return NoContent();
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Owner")]
+    public async Task<IActionResult> CreateGroomer([FromBody] CreateGroomerDto dto, CancellationToken ct)
+    {
+        var salonId = currentUser.SalonId;
+        await service.CreateGroomerAsync(dto, salonId, ct);
+        return NoContent();
+    }
     
 }
--- Grooming-Management-App/Controllers/SalonController.cs
+++ Grooming-Management-App/Controllers/SalonController.cs
@@ -1,6 +1,30 @@
-﻿namespace Grooming_Management_App.Controllers;
+﻿using Grooming_Management_App.DTOs.SalonDTO;
+using Grooming_Management_App.Services.CurrentUserServ;
+using Grooming_Management_App.Services.SalonServ;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class SalonController
+namespace Grooming_Management_App.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SalonController(ISalonService service, ICurrentUserService currentUser) : ControllerBase
 {
+    [HttpGet]
+    [Authorize(Roles = "Owner")]
+    public async Task<GetSalonDto> GetSalon(CancellationToken ct)
+    {
+        var salonId = currentUser.SalonId;
+        return await service.GetSalonAsync(salonId, ct);
+    }
+
+    [HttpPut]
+    [Authorize(Roles = "Owner")]
+    public async Task<IActionResult> UpdateSalon(UpdateSalonDto dto, CancellationToken ct)
+    {
+        var salonId = currentUser.SalonId;
+        await service.UpdateSalonAsync(dto, salonId, ct);
+        return NoContent();
+    }
     
 }
